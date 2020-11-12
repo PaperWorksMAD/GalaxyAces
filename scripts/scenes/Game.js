684ed4e47d22ca2adb62d9eb18c4939ae1e07169
JesusAyalaMatarin <jexuxayala@gmail.com>
2020-11-12 13:38:16 +0100

    disparo
    disparo provisional nave1

--- scripts/scenes/Game.js
+++ scripts/scenes/Game.js
@@ -1,10 +1,16 @@
 import { sceneManager } from "../sceneManager.js";
+
+var bullets;
+var speed;
+var lastFired = 0;
+
 export class Game extends Phaser.Scene {
     constructor() {
         super({
             key: sceneManager.SCENES.GAME
         })
     }
+
     onEvent() {
         this.initialTime -= 1; // One second
         this.text.setText(this.formatTime(this.initialTime));
@@ -27,6 +33,49 @@ export class Game extends Phaser.Scene {
     init() {
     }
     create() {
+        //clase bala
+        var Bullet = new Phaser.Class({
+
+            Extends: Phaser.GameObjects.Image,
+    
+            initialize:
+    
+            function Bullet (scene)
+            {
+                Phaser.GameObjects.Image.call(this, scene, 0, 0, 'x');
+    
+                this.speed = Phaser.Math.GetSpeed(400, 1);
+            },
+    
+            fire: function (x, y)
+            {
+                this.setPosition(x, y - 50);
+    
+                this.setActive(true);
+                this.setVisible(true);
+            },
+    
+            update: function (time, delta)
+            {
+                this.y -= this.speed * delta;
+    
+                if (this.y < -50)
+                {
+                    this.setActive(false);
+                    this.setVisible(false);
+                }
+            }
+    
+        });
+
+        bullets = this.add.group({
+            classType: Bullet,
+            maxSize: 10,
+            runChildUpdate: true
+        });
+
+        speed = Phaser.Math.GetSpeed(300, 1);
+
         this.initialTime = 20;
 
         let xbt = this.add.image(this.game.renderer.width - 50, this.game.renderer.height - 550, "x").setDepth(2);
@@ -42,6 +91,7 @@ export class Game extends Phaser.Scene {
         this.jugador1.setCollideWorldBounds(true);
         this.cursor = this.input.keyboard.createCursorKeys();
         this.keys = this.input.keyboard.addKeys('A,W,S,D');
+        this.spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.jugador2 = this.physics.add.image(this.game.renderer.width / 2 - 150, this.game.renderer.height - 100, "nave2");
         this.jugador2.setScale(0.5);
         this.jugador2.setCollideWorldBounds(true);
@@ -49,7 +99,7 @@ export class Game extends Phaser.Scene {
         this.tiempo = this.time.addEvent({ delay: 1000, callback: this.onEvent, callbackScope: this, loop: true });
         this.tiempopartida = this.time.delayedCall(this.initialTime*1000, this.onEvent2, [], this);
     }
-    update() {
+    update(time, delta) {
 
         this.jugador1.setVelocity(0);
         this.jugador2.setVelocity(0);
@@ -82,6 +132,19 @@ export class Game extends Phaser.Scene {
             this.jugador2.setVelocityY(200);
         }
         this.fondo.tilePositionY -= 1;
+
+        if (this.spaceBar.isDown && time > lastFired){
+            console.log("disparo");
+            var bullet = bullets.get();
+
+            if (bullet)
+            {
+                bullet.fire(this.jugador2.x, this.jugador2.y);
+    
+                lastFired = time + 50;
+            }
+        }
+
     }
 
 
